52c96a3c3726aaf3fe196d5912188debad913175
Andy.Hu <992990831@qq.com>
2023-09-06 22:06:45 +0800

    update research

--- src/Scene/Research/Components/ResearchGallery/index.tsx
+++ src/Scene/Research/Components/ResearchGallery/index.tsx
@@ -2,36 +2,77 @@ import * as React from 'react';
 import './style.css';
 import { StaticImage } from 'gatsby-plugin-image'
 
-const researchData: any[] = [{
-    name: '实验室最新成果',
-    address: ' 39 Remsen St, Brooklyn, NY 11201, USA',
-    beds: 3,
-    toilets: 2,
-    square: 20,
-    img: 'https://ciiic.sjtu.edu.cn/static/upload/image/20230705/1688525171375756.png'
-}, {
-    name: '实验室最新成果',
-    address: ' 169 Warren St, Brooklyn, NY 11201, USA',
-    beds: 3,
-    toilets: 2,
-    square: 20,
-    img: 'https://ciiic.sjtu.edu.cn/static/upload/image/20230618/1687093135162401.png'
-}, {
-    name: '实验室最新成果',
-    address: ' 39 Remsen St, Brooklyn, NY 11201, USA',
-    beds: 3,
-    toilets: 2,
-    square: 20,
-    img: 'https://ciiic.sjtu.edu.cn/static/upload/image/20230626/1687791555578576.jpg'
-}];
-
 const ResearchGallery = () => {
     return (
         <div className="listProperty">
             <div className="row listPropertyContent">
                 <h1 style={{ textAlign: "center" }}>研究方向</h1>
-                
-                <h2>全脑高分辨多分子一体成像；多模态跨尺度脑图谱；脑卒中精准诊断；AD早期预警及认知预测；脑肿瘤精准诊断；精神疾病影像引导精准调控；</h2>
+                <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <div className="singleResearch">
+                        <div className="content">
+                            <StaticImage className='news'
+                                alt="研究方向"
+                                src={`../../images/全脑高分辨多分子一体成像.webp`}
+                            />
+                            <h2>{"全脑高分辨多分子一体成像"}</h2>
+                        </div>
+                    </div>
+                </div>      
+                <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <div className="singleResearch">
+                        <div className="content">
+                            <StaticImage className='news'
+                                alt="研究方向"
+                                src={`../../images/多模态跨尺度脑图谱1.webp`}
+                            />
+                            <h2>{"多模态跨尺度脑图谱"}</h2>
+                        </div>
+                    </div>
+                </div>       
+                <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <div className="singleResearch">
+                        <div className="content">
+                            <StaticImage className='news'
+                                alt="研究方向"
+                                src={`../../images/0375069256d144d4b066ed312d3c23de.webp`}
+                            />
+                            <h2>{"脑卒中精准诊断"}</h2>
+                        </div>
+                    </div>
+                </div>     
+                <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <div className="singleResearch">
+                        <div className="content">
+                            <StaticImage className='news'
+                                alt="研究方向"
+                                src={`../../images/A1421217082_small.webp`}
+                            />
+                            <h2>{"AD早期预警及认知预测"}</h2>
+                        </div>
+                    </div>
+                </div>  
+                <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <div className="singleResearch">
+                        <div className="content">
+                            <StaticImage className='news'
+                                alt="研究方向"
+                                src={`../../images/549b1b3e03da484f88ec9158a7fa7d12.webp`}
+                            />
+                            <h2>{"脑肿瘤精准诊断"}</h2>
+                        </div>
+                    </div>
+                </div>   
+                <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <div className="singleResearch">
+                        <div className="content">
+                            <StaticImage className='news'
+                                alt="研究方向"
+                                src={`../../images/精神疾病影像引导精准调控.webp`}
+                            />
+                            <h2>{"精神疾病影像引导精准调控"}</h2>
+                        </div>
+                    </div>
+                </div>                
             </div>
         </div>
         // <>
